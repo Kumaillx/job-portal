ca0d7e533ac431cff82371ff76b76854b9601f2f
Syed M.Kumail Ali <m.kumail80@gmail.com>
2025-07-25 11:55:59 +0500

    Made an array and then a map function to traveerse all the JobCards inside that particular page.

--- landing-page/app/AllJobs/page.tsx
+++ landing-page/app/AllJobs/page.tsx
@@ -10,14 +10,75 @@ import bd from '@/app/images/BD.png';
 import qa from '@/app/images/QA.png';
 import mk from '@/app/images/Marketing.png';
 import Link from 'next/link';
+import type { StaticImageData } from 'next/image';
+
+
+// Convert this structure to arrays
+
+// Use Maps to then pass a conditional opp and then navigate straight towards that particular params.
+// {
+//           a.map((k)=>(
+          
+//             (k===2?   <div className={styles.jobCard}>
+//           <Link href="/apply/frontend-developer">
+//           <Image src={fdev} alt="Frontend Developer" />
+//           <h3>Frontend Developer</h3>
+//           <p>React | Next.js | TypeScript</p>
+//           </Link>
+        
+//         </div>
+// :"")
+//           ))
+//         }
+
+
+interface JobCard {
+  href: string;
+  imageSrc: StaticImageData;
+  altText: string;
+  title: string;
+  description: string;
+}
+
+// the whole array to be stored in the jobCards variable
+const jobCards: JobCard[] = [
+  {
+    href: '/apply/frontend-developer',
+    imageSrc: fdev,
+    altText: 'Frontend Developer',
+    title: 'Frontend Developer',
+    description: 'React | Next.js | TypeScript',
+  }
+]
+
+const JobList: React.FC = () => {
+  return (
+    <div>
+      {jobCards.map((job, index) => (
+        <div key={index} className={styles.jobCard}>
+          <Link href={job.href}>
+            <Image src={job.imageSrc} alt={job.altText} />
+            <h3>{job.title}</h3>
+            <p>{job.description}</p>
+          </Link>
+        </div>
+      ))}
+    </div>
+  );
+};
 
 function JobsPage() {
   return (
+
     
+
+// The Navbar 
     <section className={styles.availableJobs}>
       <div className={styles.footerContainer}>
           <Navbar />
         </div>
+{/* The Main Job Cards Section */}
+
       <h2>Available Jobs</h2>
       <div className={styles.jobCards}>
         
